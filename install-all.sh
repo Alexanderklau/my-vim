67fe63d4c04da023bc88139548ac0d215e33c761
James Zhu <zhujian0805@gmail.com>
2015-05-05 17:45:46 +0800

    mod

--- install-all.sh
+++ install-all.sh
@@ -19,6 +19,8 @@
 
 set -o nounset                              # Treat unset variables as an error
 
+sudo apt-get install python-pip git
+
 pip install --user git+git://github.com/Lokaltog/powerline
 
 cat <<EOF >> ~/.profile
@@ -29,6 +31,6 @@ EOF
 
 
 wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
-mkdir -p ~/.fonts/ && mv PowerlineSymbols.otf ~/.fonts/
-fc-cache -vf ~/.fonts
-mkdir -p ~/.config/fontconfig/conf.d/ && mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+sudo mv PowerlineSymbols.otf /usr/share/fonts/
+sudo fc-cache -vf
+sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
